ea906d9c06a6178bc2602ea5b3853585a450c8d1
DavidLin1577 <linpeng1577@gmail.com>
2021-06-25 18:11:50 +0800

    wdt rst example

--- phoenix04_examples/example/wdt_rst_example.c
+++ phoenix04_examples/example/wdt_rst_example.c
@@ -10,26 +10,22 @@
  */
 
 #include "lib_include.h"
+#include "shell.h"
 
-#define LED_PIN GPIO_PIN8
+#define LED_PIN          GPIO_PIN8
+#define LED_ON           GPIO_SetPin(LED_PIN)
+#define LED_OFF          GPIO_ClrPin(LED_PIN)
 
-#define LED_ON GPIO_SetPin(LED_PIN)
-#define LED_OFF GPIO_ClrPin(LED_PIN)
-
-int wdt_rst_example(void)
+int wdt_rst_example(int delay)
 {
-    int i, j;
-    printf("test\r\n");
     GPIO_PinConfigure(LED_PIN, DISABLE, ENABLE, ENABLE, DISABLE, DISABLE);
-    for (i = 0; i < 4; ++i) {
-        LED_ON;
-        for (j = 1000000; j > 0; j--)
-            ;
-        LED_OFF;
-        for (j = 1000000; j > 0; j--)
-            ;
-    }
-    WDT_Init(4, LP_CLKSEL_LRC, WDT_OV_RST);
-    while (1)
-        ;
+
+    LED_ON;
+
+    WDT_Init(delay, LP_CLKSEL_LRC, WDT_OV_RST);
+
+    WDT_StartCount();
+
+    return 0;
 }
+SHELL_EXPORT_CMD(wdt_rst_example, wdt_rst_example, input delay<0-xxx>);
