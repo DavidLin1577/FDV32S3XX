77ee942b04cc0dd9de1564987f555ff1f82d9deb
DavidLin1577 <linpeng1577@gmail.com>
2021-06-27 17:00:25 +0800

    [drivers]update spi.h and timer.h

--- phoenix04_examples/FDV32S304/drivers/spi.h
+++ phoenix04_examples/FDV32S304/drivers/spi.h
@@ -1,51 +1,54 @@
 /**
  * @file spi.h
- * @author bifei.tang
+ * @author 
  * @brief
  * @version 0.1
- * @date 2020-05-12
+ * @date 2021-06-27
  *
- * @copyright Fanhai Data Tech. (c) 2020
+ * @copyright Fanhai Data Tech. (c) 2021
  *
  */
 
-#ifndef __SPI_H
-#define __SPI_H
+#ifndef __SPI_H__
+#define __SPI_H__
+
 #include "phnx04.h"
 
 // reg bit definitions
 // SPI CR0
-#define SPI_CR0_CPOL 	BIT(7)
-#define SPI_CR0_CPHA 	BIT(6)
-#define SPI_CR0_MSTCFIE BIT(5)
-#define SPI_CR0_DONEIE 	BIT(4)
-#define SPI_CR0_MSMODE 	BIT(3)
-#define SPI_CR0_SCLKDIV BITS(0, 2)
+#define SPI_CR0_CPOL 	      BIT(7)
+#define SPI_CR0_CPHA 	      BIT(6)
+#define SPI_CR0_MSTCFIE       BIT(5)
+#define SPI_CR0_DONEIE 	      BIT(4)
+#define SPI_CR0_MSMODE 	      BIT(3)
+#define SPI_CR0_SCLKDIV       BITS(0, 2)
 
-#define SPI_CR0_SCLKDIV_pos (0) // 0-7 div(2^(x+1))
+#define SPI_CR0_SCLKDIV_pos   (0) // 0-7 div(2^(x+1))
 
 // SPI SR
-#define SPI_SR_MSTCFSR 	BIT(1)
-#define SPI_SR_DONESR 	BIT(0)
+#define SPI_SR_MSTCFSR 	      BIT(1)
+#define SPI_SR_DONESR 	      BIT(0)
 
 // extend define
-#define SPI_CPOL_LOW 	0
-#define SPI_CPOL_HIGH 	1
+#define SPI_CPOL_LOW 	      (0)
+#define SPI_CPOL_HIGH         (1)
 
-#define SPI_CPHA_FIST 	0
-#define SPI_CPHA_MIDD 	1
+#define SPI_CPHA_FIST 	      (0)
+#define SPI_CPHA_MIDD 	      (1)
 
-#define SPI_SLAVE 		0
-#define SPI_MASTER 		1
+#define SPI_SLAVE 		      (0)
+#define SPI_MASTER 		      (1)
 
-#define SPI_ClrIntFlag()                                                       \
-    { int tmp = SPI->SR; }
+#define SPI_ClrIntFlag()      \
+{                             \ 
+    int tmp = SPI->SR;        \          
+}
 
-#define SPI_PIN_12_13_16_17 0
-#define SPI_PIN_14_15_19_18 1
+#define SPI_PIN_12_13_16_17   (0)
+#define SPI_PIN_14_15_19_18   (1)
 
 // extern function declare
-void SPI_Init(int pin, int mode, int pol, int phase, int freq);
+void SPI_Init(int pin, int mode, int pol, int phase, int freq);                                                                                                                                                                      
 void SPI_DeInit(void);
 void SPI_MasterConflictIRQControl(ControlStatus ctl);
 void SPI_EnableIRQ(void);
@@ -53,6 +56,7 @@ void SPI_DisableIRQ(void);
 void SPI_SetCSN(void);
 void SPI_ClrCSN(void);
 void SPI_SendData(u8 data);
-u8 SPI_RecieveData(void);
-u32 SPI_GetStatus(void);
-#endif
+u8   SPI_RecieveData(void);
+u32  SPI_GetStatus(void);
+
+#endif /*__SPI_H__*/
--- phoenix04_examples/FDV32S304/drivers/timer.h
+++ phoenix04_examples/FDV32S304/drivers/timer.h
@@ -3,62 +3,63 @@
  * @author bifei.tang
  * @brief
  * @version 0.1
- * @date 2020-05-12
+ * @date 2020-06-27
  *
- * @copyright Fanhai Data Tech. (c) 2020
+ * @copyright Fanhai Data Tech. (c) 2021
  *
  */
-#ifndef __TIM_H
-#define __TIM_H
+#ifndef __TIM_H__
+#define __TIM_H__
+
 #include "phnx04.h"
 
 // reg bit definitions
 // TIM PWCON
-#define TIM_PWCON_PWMCPOL 	BITS(17, 18)
-#define TIM_PWCON_DTEN 		BITS(16)
-#define TIM_PWCON_DTUGAP 	BITS(0, 15)
+#define TIM_PWCON_PWMCPOL 	        BITS(17, 18)
+#define TIM_PWCON_DTEN 		        BITS(16)
+#define TIM_PWCON_DTUGAP 	        BITS(0, 15)
 
-#define TIM_PWCON_PWMCPOL_pos 	(17) // 0-3
-#define TIM_PWCON_DTUGAP_pos 	(0) // 0-0xffff
+#define TIM_PWCON_PWMCPOL_pos 	    (17) // 0-3
+#define TIM_PWCON_DTUGAP_pos 	    (0) // 0-0xffff
 
 // TIM CON
-#define TIM_CON_PAUSE 		BITS(24, 25)
-#define TIM_CON_PAUSE_TIM1 	BIT(24)
-#define TIM_CON_PAUSE_TIM2 	BIT(25)
+#define TIM_CON_PAUSE 		        BITS(24, 25)
+#define TIM_CON_PAUSE_TIM1      	BIT(24)
+#define TIM_CON_PAUSE_TIM2      	BIT(25)
 
-#define TIM_CON_EXTPOL 		BITS(20, 21)
-#define TIM_CON_EXTPOL_TIM1 BIT(20)
-#define TIM_CON_EXTPOL_TIM2 BIT(21)
+#define TIM_CON_EXTPOL 		        BITS(20, 21)
+#define TIM_CON_EXTPOL_TIM1         BIT(20)
+#define TIM_CON_EXTPOL_TIM2         BIT(21)
 
-#define TIM_CON_EXTEN 		BITS(16, 17)
-#define TIM_CON_EXTEN_TIM1 	BIT(16)
-#define TIM_CON_EXTEN_TIM2 	BIT(17)
+#define TIM_CON_EXTEN 		        BITS(16, 17)
+#define TIM_CON_EXTEN_TIM1 	        BIT(16)
+#define TIM_CON_EXTEN_TIM2 	        BIT(17)
 
-#define TIM_CON_PWM 		BITS(12, 13)
-#define TIM_CON_PWM_TIM1 	BIT(12)
-#define TIM_CON_PWM_TIM2 	BIT(13)
+#define TIM_CON_PWM 		        BITS(12, 13)
+#define TIM_CON_PWM_TIM1 	        BIT(12)
+#define TIM_CON_PWM_TIM2 	        BIT(13)
 
-#define TIM_CON_IE 			BITS(8, 9)
-#define TIM_CON_IE_TIM1 	BIT(8)
-#define TIM_CON_IE_TIM2 	BIT(9)
+#define TIM_CON_IE 			        BITS(8, 9)
+#define TIM_CON_IE_TIM1 	        BIT(8)
+#define TIM_CON_IE_TIM2 	        BIT(9)
 
-#define TIM_CON_TM 			BITS(4, 5)
-#define TIM_CON_TM_TIM1 	BIT(4)
-#define TIM_CON_TM_TIM2 	BIT(5)
+#define TIM_CON_TM 			        BITS(4, 5)
+#define TIM_CON_TM_TIM1 	        BIT(4)
+#define TIM_CON_TM_TIM2 	        BIT(5)
 
-#define TIM_CON_TE 			BITS(0, 1)
-#define TIM_CON_TE_TIM1 	BIT(0)
-#define TIM_CON_TE_TIM2 	BIT(1)
+#define TIM_CON_TE 			        BITS(0, 1)
+#define TIM_CON_TE_TIM1 	        BIT(0)
+#define TIM_CON_TE_TIM2 	        BIT(1)
 
 // TIM INTFLAG
-#define TIM_INTFLAG 		BITS(0, 1)
-#define TIM_INTFLAG_TIM1 	BIT(0)
-#define TIM_INTFLAG_TIM2 	BIT(1)
+#define TIM_INTFLAG 		        BITS(0, 1)
+#define TIM_INTFLAG_TIM1 	        BIT(0)
+#define TIM_INTFLAG_TIM2 	        BIT(1)
 
 // TIM INTCLR
-#define TIM_INTCLR 			BITS(0, 1)
-#define TIM_INTCLR_TIM1 	BIT(0)
-#define TIM_INTCLR_TIM2 	BIT(1)
+#define TIM_INTCLR 			        BITS(0, 1)
+#define TIM_INTCLR_TIM1 	        BIT(0)
+#define TIM_INTCLR_TIM2 	        BIT(1)
 
 // extend define
 #define TIM_TM_AUTO_RUN 			0
@@ -85,8 +86,7 @@
 
 void TIM_TimerInit(TIM_Type *pTim,int del);
 void TIM_CounterInit(TIM_Type *pTim, int cntPolarity, int portSel);
-void TIM_PWMInit(TIM_Type *pTim, int pwmPolarity, int freq, int duty,
-                 int portSel, int dtGap);
+void TIM_PWMInit(TIM_Type *pTim, int pwmPolarity, int freq, int duty, int portSel, int dtGap);
 void TIM_DeInit(TIM_Type *pTim);
 void TIM_EnableControl(TIM_Type *pTim, int iCtrl);
 void TIM_EnableIRQ(TIM_Type *pTim);
@@ -95,4 +95,4 @@ void TIM_PauseCntControl(TIM_Type *pTim, ControlStatus ctl);
 FlagStatus TIM_GetIntFlag(TIM_Type *pTim);
 void TIM_ClrIntFlag(TIM_Type *pTim);
 
-#endif
+#endif /*__TIM_H__*/
