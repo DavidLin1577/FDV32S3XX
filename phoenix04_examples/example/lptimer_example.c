67be7ff42f71182331e42520a1b00c2091c21edd
DavidLin1577 <linpeng1577@gmail.com>
2021-06-28 11:10:15 +0800

    [drivers]解决lptimer时钟使能问题

--- phoenix04_examples/example/lptimer_example.c
+++ phoenix04_examples/example/lptimer_example.c
@@ -10,30 +10,39 @@
  */
 
 #include "lib_include.h"
+#include "shell.h"
 
-#define LED_PIN GPIO_PIN8
+#define LED_PIN    GPIO_PIN8
+#define LED_ON     GPIO_SetPin(LED_PIN)
+#define LED_OFF    GPIO_ClrPin(LED_PIN)
 
-#define LED_ON GPIO_SetPin(LED_PIN)
-#define LED_OFF GPIO_ClrPin(LED_PIN)
-
-void LPT_IrqHandler(void) {
+void LPT_IrqHandler(void) 
+{
     static int tog = 0;
-    if (tog) {
+    if (tog) 
+    {
         LED_ON;
         tog = 0;
-    } else {
+    } else 
+    {
         LED_OFF;
         tog = 1;
     }
     LPT_ClrIntFlag();
+    printf("tog %x\n", tog);
 };
 
-int lptimer_example(void)
+int lptimer_example(int delay)
 {
     GPIO_PinConfigure(LED_PIN, DISABLE, ENABLE, ENABLE, DISABLE, DISABLE);
-    LPT_Init(LP_CLKSEL_LRC, 4, LPT_PIT_CNT);
+    LPT_Init(LP_CLKSEL_LRC, delay, LPT_PIT_CNT);
     PLIC_EnableIRQ(LPT_IRQn);
     PLIC_SetPriority(LPT_IRQn, 1);
     LPT_ClrIntFlag();
     LPT_EnableIRQ();
+
+    LPT_EnableControl(ENABLE);
+
+    return delay;
 }
+SHELL_EXPORT_CMD(lptimer_example, lptimer_example, input delay<0 - xxx>);
