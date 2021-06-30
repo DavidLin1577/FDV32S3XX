3115f27997fd0dfa9e8335c97dd7b2af2e6c92fa
DavidLin1577 <linpeng1577@gmail.com>
2021-06-30 10:34:42 +0800

    增加spi example基础模板

---
+++ phoenix04_examples/example/spi_master_example.c
@@ -0,0 +1,19 @@
+/**
+ * @file spi_master_example.c
+ * @author david.lin 
+ * @brief
+ * @version 1.0
+ * @date 2021-06-30
+ *
+ * @copyright Fanhai Data Tech. (c) 2021
+ *
+ */
+
+#include "lib_include.h"
+#include "shell.h"
+
+int spi_master_example(void)
+{
+    return 0;
+}
+SHELL_EXPORT_CMD(spi_master_example, spi_master_example, spi master example);
